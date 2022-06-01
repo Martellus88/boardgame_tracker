7e7a778fb4936fcac9e756e4446fcbf43c9e06a7
Marat <helvegen82@gmail.com>
2022-06-01 18:02:11 +0300

    create user model and migrations

--- boardgame_tracker/boardgame_tracker/settings.py
+++ boardgame_tracker/boardgame_tracker/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# My settings
+AUTH_USER_MODEL = 'auth_user.User'
