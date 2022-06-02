9d26d0049c2c0ddd04c84d90c43c5d15d4b50c54
Marat <helvegen82@gmail.com>
2022-06-02 19:43:01 +0300

    created models and migration

--- boardgame_tracker/bg_tracker/admin.py
+++ boardgame_tracker/bg_tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Player, Statistic, Score
+
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Score)
+admin.site.register(Statistic)
--- boardgame_tracker/bg_tracker/models.py
+++ boardgame_tracker/bg_tracker/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+
+from auth_user.models import User
+
+
+class Game(models.Model):
+    game_name = models.CharField(max_length=255, unique=True)
+    image = models.URLField()
+    slug = models.SlugField(max_length=255)
+    user_id = models.ManyToManyField(User)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.game_name)
+
+    def __str__(self):
+        return self.game_name
+
+
+class Player(models.Model):
+    username = models.CharField(max_length=100)
+    user_friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_friends')
+    game_played = models.ManyToManyField(Game, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Statistic(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_owner')
+    stats_name = models.CharField(max_length=100)
+    game_date = models.DateField(default=timezone.now)
+    duration = models.TimeField(default=datetime.time())
+    comments = models.TextField(blank=True, null=True)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game_stat')
+    winner = models.ForeignKey(Player, on_delete=models.PROTECT, related_name='winner')
+    players = models.ManyToManyField(Player)
+
+    def __str__(self):
+        return f"{self.game_date}-{self.game}: winner - {self.winner}"
+
+
+class Score(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player')
+    stats = models.ForeignKey(Statistic, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"*{self.player}* in [{self.stats.stats_name}] with score: {self.score}"
