552acef30ef78fa343103e59106bbcea79237d4b
Marat <helvegen82@gmail.com>
2022-06-02 19:51:13 +0300

    added home page view and template

--- boardgame_tracker/bg_tracker/views.py
+++ boardgame_tracker/bg_tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, View, CreateView, DetailView
 
-# Create your views here.
+
+class HomePage(View):
+
+    def get(self, request):
+        return render(request, 'bg_tracker/index.html')
